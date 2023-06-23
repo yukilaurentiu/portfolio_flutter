5f7d664fffc8fd0a157e0573cee18f454be2eb18
yuki <yuki.laurentiu@gmail.com>
2023-06-22 21:46:29 -0600

    added demo image

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter/components/nav_menubutton.dart';
-import 'section_card.dart';
+
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -18,16 +18,69 @@ class _MainPageState extends State<MainPage> {
           actions: <Widget>[
             Row(
               children: [
-                NavMenuButton(onPress: (){ Navigator.pushNamed(context, '/home');}, menuText: 'Home'),
-
-                  ],),
+                NavMenuButton(
+                    onPress: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    menuText: 'Home'),
+                SizedBox(
+                  width: 10.0,
+                ),
+                NavMenuButton(
+                    onPress: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    menuText: 'About'),
+                SizedBox(
+                  width: 10.0,
+                ),
+                NavMenuButton(
+                    onPress: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    menuText: 'Project'),
+                SizedBox(
+                  width: 10.0,
+                ),
+                NavMenuButton(
+                    onPress: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    menuText: 'Contact'),
+                SizedBox(
+                  width: 10.0,
+                ),
               ],
             ),
-        body: const Column(
+          ],
+        ),
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Expanded(
-              child: SectionCard(),
+            Row(
+              children: [
+                SizedBox(width: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image(
+                    image: AssetImage('images/user_kitty.jpg'),
+                  ),
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  children: [
+                    Text(
+                      'Yukiko Laurentiu',
+                      style: TextStyle(
+                        fontSize: 40.0, fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('Full stack developer',style: TextStyle(
+                      fontSize: 20.0,
+                    ), ),
+                  ],
+                ),
+              ],
             ),
           ],
         ));
