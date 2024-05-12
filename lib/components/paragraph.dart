e470fe4529713d864d1a7114cb0fe696fe3c44e4
yuki <yuki.laurentiu@gmail.com>
2024-05-12 06:14:02 +0200

    changed

--- lib/components/paragraph.dart
+++ lib/components/paragraph.dart
@@ -1,5 +1,5 @@
 const travelMap =
-    'This is a final project from Le Wagon Bootcamp, creating an app using Ruby on Rails with MVC-pattern, '
+    'This is a final project from Le Wagon Bootcamp, using Ruby on Rails with MVC-pattern, '
     'application authentication, authorization for security, Calendar method, Chat function, '
     'and Map method with marker functions in JavaScript.';
 
